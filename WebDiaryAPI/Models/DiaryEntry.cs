6feef3cf8a64525bd5652fd2b8bc81ed97224e1d
pedrohgarbim <phg.garbim@gmail.com>
2024-09-18 16:15:42 -0300

    Db again

---
+++ WebDiaryAPI/Models/DiaryEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDiaryAPI.Models
+{
+	public class DiaryEntry
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Content { get; set; }
+		public DateTime Created { get; set; } = DateTime.Now;
+	}
+}
